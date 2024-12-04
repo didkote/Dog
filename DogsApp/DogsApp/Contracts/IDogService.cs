0e8cd7e93e50f052a7e0a4804429afe63fae58f5
didkote <didostav@abv.bg>
2024-12-04 12:06:06 +0200

    completed task 3

---
+++ DogsApp/DogsApp/Contracts/IDogService.cs
@@ -0,0 +1,16 @@
+﻿using DogsApp.Infrastructure.Data.Domain;
+
+namespace DogsApp.Contracts
+{
+    public interface IDogService
+    {
+        bool Create(string name , int age, string breed, string picture);
+        bool UpdateDog(int dogId, string name, int age, string breed, string picture);
+        bool Uploading (int dogId, string name, int age, string breed, string picture);
+        List<Dog> GetDogs();
+        Dog GetDogById(int dogId);
+        bool RemoveById(int dogId);
+        List<Dog> GetDogs(string searchStringBreed, string searchStringName);
+
+    }
+}
